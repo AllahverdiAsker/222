1c3b1bc2c9621c1c597cc974f55a19c9dfe7aae1
Allahverdi Asgarli <allahverdiaa@code.edu.az>
2022-06-16 01:44:58 -0700

    222

---
+++ 222/222/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student()
+            {
+                FullName = "Hasan",
+                GroupNo = "O222"
+            };
+            Student student2 = new Student()
+            {
+                FullName = "Omer",
+                GroupNo = "O22"
+            };
+            Student student3 = new Student()
+            {
+                FullName = "Nuh",
+                GroupNo = "T77"
+            };
+
+            Console.WriteLine(student1.GroupNo);
+            Console.WriteLine(student2.GroupNo);
+            Console.WriteLine(student3.GroupNo);
+        }
+    }
+    }
+
+
